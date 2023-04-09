e97e62a45b0f2d4e77c0b284e4af2b6106c3c491
Azis R <alfatihstudi@gmail.com>
2023-04-10 06:30:22 +0700

    Custom properties

--- src/main/java/com/example/demo/rest/FunRestController.java
+++ src/main/java/com/example/demo/rest/FunRestController.java
@@ -1,17 +1,27 @@
 package com.example.demo.rest;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class FunRestController {
+
+    // inject properties
+    @Value("${coach.name}")
+    private String coachName;
+
+    @Value("${team.name}")
+    private  String teamName;
+
     @GetMapping("/")
     public String helloWorld() {
         return "Hello World!";
     }
 
-    @GetMapping("/something")
-    public  String error() {
-        return "Hello Error Again and!";
+    // expose new endpoint for team info
+    @GetMapping("/teamInfo")
+    public  String getTeamInfo() {
+        return "Coach: " + coachName + " \nTeam: " + teamName;
     }
 }

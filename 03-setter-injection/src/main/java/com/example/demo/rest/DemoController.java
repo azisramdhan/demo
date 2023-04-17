6cbbd7e0e08ade5517cc54cf0616731756c0fb64
Azis R <alfatihstudi@gmail.com>
2023-04-18 04:40:25 +0700

    Setter injection without @Autowired

--- 03-setter-injection/src/main/java/com/example/demo/rest/DemoController.java
+++ 03-setter-injection/src/main/java/com/example/demo/rest/DemoController.java
@@ -10,9 +10,7 @@ public class DemoController {
     // define the private field for the dependency
     private Coach coach;
 
-    // define a constructor for dependency injection
-    @Autowired
-    public DemoController(Coach coach) {
+    public void setCoach(Coach coach) {
         this.coach = coach;
     }
 

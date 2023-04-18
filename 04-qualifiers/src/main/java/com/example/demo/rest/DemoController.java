df8aaaf9e655d631c6f174d14bc7e9ca7cef3402
Azis R <alfatihstudi@gmail.com>
2023-04-18 19:33:45 +0700

    Add qualifiers to controller

--- 04-qualifiers/src/main/java/com/example/demo/rest/DemoController.java
+++ 04-qualifiers/src/main/java/com/example/demo/rest/DemoController.java
@@ -2,14 +2,19 @@ package com.example.demo.rest;
 
 import com.example.demo.common.Coach;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class DemoController {
     // define the private field for the dependency
-    @Autowired
     private Coach coach;
+
+    @Autowired
+    public void setCoach(@Qualifier("cricketCoach") Coach coach) {
+        this.coach = coach;
+    }
 
     @GetMapping("/dailyworkout")
     public String getDailyWorkout() {

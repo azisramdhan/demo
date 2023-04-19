81fa656cf0300d1099562f6c71a740cd2484ed62
Azis R <alfatihstudi@gmail.com>
2023-04-19 19:47:53 +0700

    [Init] Lazy initialization

---
+++ 06-lazy-initialization/src/main/java/com/example/demo/common/CricketCoach.java
@@ -0,0 +1,13 @@
+package com.example.demo.common;
+
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Component;
+
+@Component
+@Primary
+public class CricketCoach implements Coach {
+    @Override
+    public String getDailyWorkOut() {
+        return "Practice fast bowling for 15 minutes";
+    }
+}

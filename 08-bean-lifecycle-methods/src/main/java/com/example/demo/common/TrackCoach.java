0d71185339793bcf4b8bafe6f8b79253707fcd1c
Azis R <alfatihstudi@gmail.com>
2023-04-26 01:16:53 +0700

    [Init] Bean lifecycle methods

---
+++ 08-bean-lifecycle-methods/src/main/java/com/example/demo/common/TrackCoach.java
@@ -0,0 +1,14 @@
+package com.example.demo.common;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class TrackCoach implements Coach {
+    public TrackCoach() {
+        System.out.println("In constructor: " + getClass().getSimpleName());
+    }
+    @Override
+    public String getDailyWorkOut() {
+        return "Practice track";
+    }
+}
